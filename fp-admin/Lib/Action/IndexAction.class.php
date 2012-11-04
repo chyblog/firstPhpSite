da2c7579e21cbff24a5142b04fcbf7800520f4e9
洋 陈 <chenyang@chy.local>
2012-11-04 22:39:51 +0800

    add fp-admin

---
+++ fp-admin/Lib/Action/IndexAction.class.php
@@ -0,0 +1,8 @@
+<?php
+// 本类由系统自动生成，仅供测试用途
+class IndexAction extends Action {
+  public function index() {
+    echo "这个是管理员登陆页面，需要做登陆界面";
+  }
+
+}
---
+++ fp-admin/index.php
@@ -0,0 +1,7 @@
+<?php
+define( 'ABSPATH', dirname(dirname(__FILE__)) . '/' );
+define('APP_DEBUG', 'TRUE');
+define('APP_NAME','fp-admin');
+define('APP_PATH',ABSPATH . 'fp-admin/');
+
+require ABSPATH . 'ThinkPHP/ThinkPHP.php';
