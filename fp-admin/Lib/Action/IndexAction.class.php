ce7874b079f1ca12884494c347cd47744bd6b88b
洋 陈 <chenyang@chy.local>
2012-11-14 22:56:26 +0800

    Committer: 洋 陈 <chenyang@chy.local>
     On branch master
     Changes to be committed:
       (use "git reset HEAD <file>..." to unstage)
    
    	modified:   fp-admin/Lib/Action/IndexAction.class.php
    	modified:   fp-admin/Runtime/Logs/12_11_14.log
    	renamed:    fp-admin/Runtime/Cache/30a96590668ee03c290f07ec8ee994e3.php -> fp-admin/Tpl/Index/index.html
    	modified:   fp-admin/index.php

--- fp-admin/Lib/Action/IndexAction.class.php
+++ fp-admin/Lib/Action/IndexAction.class.php
@@ -2,7 +2,16 @@
 // 本类由系统自动生成，仅供测试用途
 class IndexAction extends Action {
   public function index() {
-    $this->wo = '2222fdd';
     $this->display();  }
-
+  
+  public function login() {
+    $username = $_POST["username"];
+    $password = $_POST["password"];
+    if($username == 'cjc') {
+      $this->display('main');
+    } else {
+      $this->loginFlag = 'false';
+      $this->display('index');
+    }
+  }
 }
--- fp-admin/index.php
+++ fp-admin/index.php
@@ -1,8 +1,8 @@
 <?php
 define( 'ABSPATH', dirname(dirname(__FILE__)) . '/' );
+define('APP_PATH',ABSPATH . 'fp-admin/');
+require ABSPATH . 'config.php';
 define('APP_DEBUG', 'TRUE');
 define('APP_NAME','fp-admin');
-define('APP_PATH',ABSPATH . 'fp-admin/');
 
 require ABSPATH . 'ThinkPHP/ThinkPHP.php';
-require ABSPATH . 'config.php';
